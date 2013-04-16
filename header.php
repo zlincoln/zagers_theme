d1ee66a6c47d36739c0570f61895a11f1097f238
Zachary Lincoln <zlincoln@steves9400.bluehouse.lan>
2013-04-16 17:06:05 -0400

    more work

--- header.php
+++ header.php
@@ -1,9 +1,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-
 <head>
-	<meta charset="<?php bloginfo('charset'); ?>" />
-	
+	<meta charset="<?php bloginfo('charset'); ?>"/>
 	<?php if (is_search()) { ?>
 	   <meta name="robots" content="noindex, nofollow" /> 
 	<?php } ?>
@@ -32,47 +30,27 @@
 	<meta name="keywords" content="graphic, art, graphic art, fine, fine art, brian zager, burlington vt, burlington, vermont, baltimore, baltimore md, maryland, design, graphic design">
 	
 	<link rel="shortcut icon" href="/favicon.ico">
-	
-	
-<!-- ******************** Stylesheets ********************-->
-	
-	<!-- Shadowbox CSS -->
+
+	<link href='http://fonts.googleapis.com/css?family=Roboto+Condensed' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="wp-content/themes/zagers_theme/css/shadowbox/shadowbox.css">
-	
-	<!-- Slideshow Styles -->
-	<link rel="stylesheet" type="text/css" href="wp-content/themes/zagers_theme/css/slideshow/tabs-slideshow.css" />
-	
-	<!-- Call Our Main CSS -->
 	<link rel="stylesheet" href="wp-content/themes/zagers_theme/css/bootstrap/styles.css">
-	
-<!-- ==================== END Stylesheets ====================== -->
-<!-- *********************** Scripts **************************** -->
 
-	<!-- Google Hosted jQuery Library -->
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
-
-	<!-- Shadowbox -->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>	
 	<script type="text/javascript" src="wp-content/themes/zagers_theme/js/shadowbox.js"></script>
-		
-<!-- ======================= END Script Calls ===================== -->
-<!-- ********************** document.ready init ******************* -->
-<script type="text/javascript">
-// google analytics
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-33235803-1']);
-  _gaq.push(['_trackPageview']);
 
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
+	<script type="text/javascript">
+	// google analytics
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-33235803-1']);
+	  _gaq.push(['_trackPageview']);
 
-</script>
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
 
-<script> 
-
-    $(function() { 
+	  $(function() { 
      	
      	// Initialize shadowbox
      	Shadowbox.init();
@@ -83,18 +61,16 @@
 		});
 
     });//END document.ready
-</script>
-	
+
+	</script>
+
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-
 	<?php if ( is_singular() ) wp_enqueue_script('comment-reply'); ?>
-
 	<?php wp_head(); ?>
 </head>
-
 <body <?php body_class(); ?>>
 	<div class="page-wrap">
-		<div class="container">
-			<div class="row">
+		<div class="container-fluid">
+			<div class="row-fluid">
 				<?php get_sidebar(); ?>
 				<section class="main-content span9 offset3">
